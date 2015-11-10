1b2fe29b45c9f25d380c50185fe61287ae4c33e8
Rin0o <pedrorino_31@hotmail.com>
2015-11-10 17:16:50 +0000

    EventManager
    Cenas base

--- AttendanceManagement/src/java/web/AdministratorManager.java
+++ AttendanceManagement/src/java/web/AdministratorManager.java
@@ -6,7 +6,9 @@
 package web;
 
 import dto.AttendantDTO;
+import dto.EventManagerDTO;
 import ejbs.AttendantBean;
+import ejbs.EventManagerBean;
 import java.util.List;
 import javax.ejb.EJB;
 import javax.faces.bean.ManagedBean;
@@ -23,13 +25,17 @@ public class AdministratorManager {
     
     @EJB
     private AttendantBean attendantBean;  
+    @EJB
+    private EventManagerBean eventManagerBean;
     
     private AttendantDTO newAttendant;
+    private EventManagerDTO newEventManager;
     private UIComponent component;
     
         public AdministratorManager() {
             
         newAttendant = new AttendantDTO();
+        newEventManager = new EventManagerDTO();
 
     }
     
@@ -58,6 +64,7 @@ public class AdministratorManager {
         }
     }
        
+       
        /////////////////// Getter Ans Setters \\\\\\\\\\\\\\\\\
        
     public AttendantDTO getNewAttendant() {
@@ -68,7 +75,32 @@ public class AdministratorManager {
         this.newAttendant = newAttendant;
     }
     
-        public UIComponent getComponent() {
+    //EventManager
+    
+    public String createEventManager(){
+        try{
+            eventManagerBean.create(
+                    newEventManager.getId(),
+                    newEventManager.getPassword(),
+                    newEventManager.getName(),
+                    newEventManager.getEmail());
+            return "index?faces-redirect=true";
+        } catch (Exception e) {
+            System.err.println("Error: "+ e.getMessage());
+            return null;
+        }
+    }
+    
+    public List<EventManagerDTO> getAllEventManagers(){
+        try {
+            return eventManagerBean.getAll();
+        } catch (Exception e) {
+            System.err.println("Error: " + e.getMessage());            
+            return null;
+        }
+    }
+    
+    public UIComponent getComponent() {
         return component;
     }
 
